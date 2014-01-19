edd235d8b69c063ebcdec5445d253ee8c145e995
rmaguire <ryan.e.maguire@gmail.com>
2014-01-18 23:10:41 -0600

    updated conns, yo!

--- include/dbconnopen.php
+++ include/dbconnopen.php
@@ -1,7 +1,7 @@
 <?php
 
 //production database connection
-$cnnCDD = mysqli_connect("yourlocaldatabase", "db-user", "db-password", "datadictionary")
+$cnnCDD = mysqli_connect("us-cdbr-azure-east-c.cloudapp.net", "bc6b1b30370246", "1f266f0e", "metaDB")
                     or die ("Error Connecting To The Database Because: " . mysqli_connect_error());
 
 ?>
